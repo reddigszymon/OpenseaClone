335075bad264f8b9df677a891f652b48198789fd
Szymon Reddig <mixowo@hotmail.com>
2022-09-28 20:44:39 +0200

    Adding env

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
   images: {
     domains: ["cdn.sanity.io", "gateway.ipfscdn.io"],
   },
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_TOKEN: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+    NEXT_PUBLIC_CMC_API_KEY: process.env.NEXT_PUBLIC_CMC_API_KEY,
+  },
 };
 
 module.exports = nextConfig;
